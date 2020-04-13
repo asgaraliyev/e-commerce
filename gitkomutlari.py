e7c38d3334beb7423f6b327d3d42642f3729a3ef
asgarliyev <asgaraliyev2001@gmail.com>
2020-04-13 15:53:50 +0400

    git komutlari geldi search axtar buttonu deyisdirildi

---
+++ gitkomutlari.py
@@ -0,0 +1,23 @@
+git config --global user.name "FIRST_NAME LAST_NAME" 
+# repositorye adinizi config eder
+git config --global user.email "MY_NAME@example.com"
+# repositorye mailinizigit config eder
+git config user.name
+# username test etme
+git config user.emaili
+# emaili test etme
+git status
+# faylda deyisiklikleri gosterer
+git add . 
+# butun fayllari ara eraziye yigar
+git add fayladi
+# fayliadini faylini ara eraziye yigar
+git commit -m "commit aciglamasi"
+# ara erazideki deyisikleri uzak repositorye kocurer
+git commit -am "commit aciglamasi"
+# auto olarag ara eraziye kocurub sora uzak reposotoriye add etmeden kocurer
+git remote add origin "repository linki"
+# remote elave etme
+git remote -v
+# remote u test etme
+
